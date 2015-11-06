18152af2a00e4a8faeadab6780d8b3d66ea94a43
tjlane <thomas.joseph.lane@gmail.com>
2015-11-05 19:31:24 -0800

    fixed sif reader

--- electrolysis/inout.py
+++ electrolysis/inout.py
@@ -76,73 +76,45 @@ class BerkeleyDat(object):
     def images(self):
         return self._images
 
-"""andor.py version 1.0
-November 2008
-Michael V. DePalatis <surname at gmail dot com>
 
-Contains functions to handle Andor SIF image files including
-conversion to other formats and reading basic information from the SIF
-file. Little is done in the way of error checking, but this is
-probably not a real issue as long as you actually pass it a valid SIF
-file.
-"""
-
-import sys
-import numpy as np
-
-class SifFile:
-    """SifFile is the Python representation of an Andor SIF image
+class SifFile(object):
+    """
+    SifFile is the Python representation of an Andor SIF image
     file. Image data is stored in a numpy array indexed as [row,
-    column] instead of [x, y]."""
+    column] instead of [x, y].
 
-    def __init__(self, path=""):
-        self.data = 0
-        if path != "":
-            self.open(path)
+    original code:
+    November 2008
+    Michael V. DePalatis <surname at gmail dot com>
 
-    def __add__(self, other):
-        new_sif = self.__class__()
-        new_sif.__dict__ = self.__dict__
-        if isinstance(other, (SifFile, np.ndarray)):
-            new_sif.data = new_sif.data + other.data
-        elif isinstance(other, (int, float)):
-            new_sif.data = new_sif.data + other
-        else:
-            raise TypeError("Addition of SIF data requires another SifFile instance, a numpy array, or a scalar.")
-        return new_sif
+    modifications:
+    November 2015
+    TJ Lane <tjlane@slac.stanford.edu>
 
-    def __sub__(self, other):
-        new_sif = self.__class__()
-        new_sif.__dict__ = self.__dict__
-        if isinstance(other, (SifFile, np.ndarray)):
-            new_sif.data = new_sif.data - other.data
-        elif isinstance(other, (int, float)):
-            new_sif.data = new_sif.data - other
-        else:
-            raise TypeError("Subtraction of SIF data requires another SifFile instance, a numpy array, or a scalar.")
-        return new_sif
+    
+    """
 
-    def __mul__(self, other):
-        new_sif = self.__class__()
-        new_sif.__dict__ = self.__dict__
-        if isinstance(other, (SifFile, np.ndarray)):
-            new_sif.data = new_sif.data * other.data
-        elif isinstance(other, (int, float)):
-            new_sif.data = new_sif.data * other
-        else:
-            raise TypeError("Multiplcation of SIF data requires another SifFile instance, a numpy array, or a scalar.")
-        return new_sif
+    def __init__(self, path):
+        self.open(path)
+        return
 
-    def __rmul__(self, other):
-        return self.__mul__(other)
 
     def open(self, path):
         """Opens the SIF file at path and stores the data in
         self.data."""
-        sif = open(path, "rb")
+
+        if path.lower().endswith('.sif'):
+            opener = open
+        elif path.lower().endswith('.sif.bz2'): # bz2 compressed sif files
+            import bz2
+            opener = bz2.BZ2File
+        else:
+            raise ValueError('Wrong extension.')
+
+        sif = opener(path, 'rb')
 
         # Verify we have a SIF file
-        if sif.readline().strip() != "Andor Technology Multi-Channel File":
+        if sif.readline().strip() != b"Andor Technology Multi-Channel File":
             sif.close()
             raise Exception("File %s is not an Andor SIF file." % path)
 
@@ -157,28 +129,17 @@ class SifFile:
         shape = sif.readline().split()
         self.ccd_size = (int(shape[0]), int(shape[1]))
 
-        # Ignore next 17 lines prior to superpixeling information
-        for i in range(17):
-            sif.readline()
-
         # Read superpixeling data
         line = sif.readline().split()
-        #self.shape = (self.ccd_size[1]/int(line[5]), self.ccd_size[0]/int(line[6]))
-
-        # Skip next line
-        for i in range(1):
-            sif.readline()
 
         # Read data
-        self.data = np.fromstring(sif.read(), dtype=np.float32)
-        self.data = self.data[:len(self.data)-2]
-        if line[3] < line[2]:
-            self.shape = (len(self.data)/int(line[3]), int(line[3]))
-        else:
-            # I'm not sure if this is correct...
-            # Needs more testing.
-            self.shape = (int(line[2]), len(self.data)/int(line[2]))
-        self.data = np.reshape(self.data, self.shape)
+        raw_data = sif.read()
+        raw_data = raw_data[len(raw_data) - 4*np.prod(self.ccd_size):]
+        self.data = np.fromstring(raw_data, dtype=np.float32)
+        self.data = np.reshape(self.data, self.ccd_size)
+
+        return
+
 
 
 
